2dd4e32389989a3aee74835e731471ad17d6b2cc
KUB4W16 <53148406+KUB4W16@users.noreply.github.com>
2021-06-16 08:18:30 +0200

    formatting fixes

--- static/src/gallery.js
+++ static/src/gallery.js
@@ -1,20 +1,19 @@
-window.addEventListener("load", () => {
-    const galleryParent = document.getElementById("galleryWrapper");
-    const galleryDisplay = document.getElementById("galleryDisplay");
+window.addEventListener('load', () => {
+    const galleryParent = document.getElementById('galleryWrapper');
+    const galleryDisplay = document.getElementById('galleryDisplay');
     var imageSources = [];
     images.forEach((link) => {
-      galleryParent.innerHTML += `<div class="gallery__item"><img src="${
-        link + "=w1920"
-      }"/></div>`;
-      imageSources.push(link);
+        galleryParent.innerHTML += `<div class="gallery__item"><img src="${
+            link + '=w1920'
+        }"/></div>`;
+        imageSources.push(link);
     });
     galleryDisplay.src = imageSources[0];
     imageElements = document
-      .querySelectorAll(".gallery__item")
-      .forEach((image) => {
-        image.addEventListener("click", () => {
-          galleryDisplay.src = image.childNodes[0].src;
+        .querySelectorAll('.gallery__item')
+        .forEach((image) => {
+            image.addEventListener('click', () => {
+                galleryDisplay.src = image.childNodes[0].src;
+            });
         });
-      });
 });
-
