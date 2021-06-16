0e8795dfe2a9d2b625e83d0d2cc5ef77463db031
KUB4W16 <53148406+KUB4W16@users.noreply.github.com>
2021-06-16 08:34:26 +0200

    formatting fixes

--- static/src/animations.js
+++ static/src/animations.js
@@ -1,58 +1,58 @@
 window.addEventListener('load', () => {
-    const el = document.querySelector('.banner');
-    const image = document.querySelector('.article__image');
-    const articles = document.querySelectorAll('#article');
-    gsap.registerPlugin(ScrollTrigger);
+  const el = document.querySelector('.banner');
+  const image = document.querySelector('.article__image');
+  const articles = document.querySelectorAll('#article');
+  gsap.registerPlugin(ScrollTrigger);
 
-    function sleep(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  sleep(250).then(() => {
+    gsap.fromTo(
+      el,
+      { autoAlpha: 1 },
+      {
+        autoAlpha: 0,
+        duration: 1,
+      }
+    );
+  });
+
+  if (image.children.length == articles.length) {
+    for (var i = 0; i < image.children.length; i++) {
+      gsap.to(image.children[i], {
+        scrollTrigger: {
+          toggleActions: 'restart none none reverse',
+          toggleClass: {
+            targets: image.children[i],
+            className: 'active-image',
+          },
+          trigger: articles[i],
+          scroller: '.article__text',
+          start: 'top 10%',
+          end: 'bottom 7.3%',
+        },
+      });
     }
+  }
 
-    sleep(250).then(() => {
-        gsap.fromTo(
-            el,
-            { autoAlpha: 1 },
-            {
-                autoAlpha: 0,
-                duration: 1,
-            }
-        );
-    });
-
-    if (image.children.length == articles.length) {
-        for (var i = 0; i < image.children.length; i++) {
-            gsap.to(image.children[i], {
-                scrollTrigger: {
-                    toggleActions: 'restart none none reverse',
-                    toggleClass: {
-                        targets: image.children[i],
-                        className: 'active-image',
-                    },
-                    trigger: articles[i],
-                    scroller: '.article__text',
-                    start: 'top 10%',
-                    end: 'bottom 7.3%',
-                },
-            });
+  document.querySelectorAll('a').forEach((a) => {
+    a.addEventListener('click', (e) => {
+      e.preventDefault();
+      var url = e.target.getAttribute('href');
+      gsap.fromTo(
+        el,
+        { autoAlpha: 0 },
+        {
+          autoAlpha: 1,
+          duration: 1,
+          onComplete: async () => {
+            await sleep(250);
+            location.href = url;
+          },
         }
-    }
-
-    document.querySelectorAll('a').forEach((a) => {
-        a.addEventListener('click', (e) => {
-            e.preventDefault();
-            var url = e.target.getAttribute('href');
-            gsap.fromTo(
-                el,
-                { autoAlpha: 0 },
-                {
-                    autoAlpha: 1,
-                    duration: 1,
-                    onComplete: async () => {
-                        await sleep(250);
-                        location.href = url;
-                    },
-                }
-            );
-        });
+      );
     });
+  });
 });
--- static/src/gallery.js
+++ static/src/gallery.js
@@ -1,19 +1,19 @@
 window.addEventListener('load', () => {
-    const galleryParent = document.getElementById('galleryWrapper');
-    const galleryDisplay = document.getElementById('galleryDisplay');
-    var imageSources = [];
-    images.forEach((link) => {
-        galleryParent.innerHTML += `<div class="gallery__item"><img src="${
-            link + '=w1920'
-        }"/></div>`;
-        imageSources.push(link);
+  const galleryParent = document.getElementById('galleryWrapper');
+  const galleryDisplay = document.getElementById('galleryDisplay');
+  var imageSources = [];
+  images.forEach((link) => {
+    galleryParent.innerHTML += `<div class="gallery__item"><img src="${
+      link + '=w1920'
+    }"/></div>`;
+    imageSources.push(link);
+  });
+  galleryDisplay.src = imageSources[0];
+  imageElements = document
+    .querySelectorAll('.gallery__item')
+    .forEach((image) => {
+      image.addEventListener('click', () => {
+        galleryDisplay.src = image.childNodes[0].src;
+      });
     });
-    galleryDisplay.src = imageSources[0];
-    imageElements = document
-        .querySelectorAll('.gallery__item')
-        .forEach((image) => {
-            image.addEventListener('click', () => {
-                galleryDisplay.src = image.childNodes[0].src;
-            });
-        });
 });
